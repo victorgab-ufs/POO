aeda43f7bb203ddb7fb4020374104a6b3db9b05a
Victor Gabriel <victor.alves@dcomp.ufs.br>
2025-10-15 15:39:03 -0300

    introdução

--- java/teoria/0.introducao.java
+++ java/teoria/0.introducao.java
@@ -1,1 +1,19 @@
+//Java possui garbage collector (coleta de lixo - memória - automática)
 
+// linguagem tipada 
+
+// fortemente voltada à orientação a objetos
+
+// compilada 
+
+// compilador -> traduz texto (código) em linguagem de máquina (binária) / verifica erros sintáticos (;) e semânticos (tipagem)
+
+// interpretador -> executa o código diretamente
+
+// class XX {}
+
+// código em java -> compilador -> programa em linguagem intermediária (bytecodes) -> JVM (Java Virtual Machine)
+// códigos mais utilizados são compilados
+
+// Java Development Kit (compilador e máquina virtual)
+// JRE
